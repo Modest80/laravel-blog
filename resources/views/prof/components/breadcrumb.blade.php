99d5a9f851f0e22acbe1d1669a35509bea572a28
Modest80 <modesting@gmail.com>
2018-04-18 17:22:35 +0400

    FIX bootstrap class name

--- resources/views/prof/components/breadcrumb.blade.php
+++ resources/views/prof/components/breadcrumb.blade.php
@@ -1,5 +1,7 @@
 <h2>{{$title}}</h2>
+<nav aria-label="breadcrumb">
 <ol class="breadcrumb">
-  <li><a href="{{route('prof.index')}}">{{$parent}}</a></li>
-  <li class="active">{{$active}}</li>
+  <li class="breadcrumb-item"><a href="{{route('prof.index')}}">{{$parent}}</a></li>
+  <li class="breadcrumb-item active">{{$active}}</li>
 </ol>
+</nav>
